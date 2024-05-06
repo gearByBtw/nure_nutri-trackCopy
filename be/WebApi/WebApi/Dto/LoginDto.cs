80c2149133e7bb079f2cf70484abf836d614bb95
Daryna <darinahanina04@gmail.com>
2024-05-06 11:34:47 +0200

    Initial creating

---
+++ be/WebApi/WebApi/Dto/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Dto;
+
+public class LoginDto
+{
+    [Required(ErrorMessage = "Email address is required")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
---
+++ be/WebApi/WebApi/Dto/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Dto;
+
+public class RegisterDto
+{
+    [Required(ErrorMessage = "Firstname is required")]
+    public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Lastname is required")]
+    public string LastName { get; set; }
+    
+    [Required(ErrorMessage = "Email address is required")]
+    [DataType(DataType.EmailAddress)]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
