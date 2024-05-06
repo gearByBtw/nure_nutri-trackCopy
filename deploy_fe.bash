0bed0688454763c52ca05bebce932800c481de9f
svyatoslav.golovin.2005@gmail.com <svyatoslav.golovin.2005@gmail.com>
2024-05-06 21:09:53 +0300

    Deploying to GitHub Pages: fix_deploy

--- deploy_fe.bash
+++ deploy_fe.bash
@@ -1,45 +1,13 @@
 #!/bin/bash
 
 cd fe
-
-if [ $? -ne 0 ]; then
-    echo "Error: 'fe' directory does not exist."
-    exit 1
-fi
-
 npm run build
-
-if [ $? -ne 0 ]; then
-    echo "Error: npm build failed."
-    exit 1
-fi
-
 cp -r dist/* ../docs
-
-if [ $? -ne 0 ]; then
-    echo "Error: Copy operation failed."
-    exit 1
-fi
-
 cd ..
 
 git add .
-
 git commit -m "Deploying to GitHub Pages: $1"
-
-if [ $? -ne 0 ]; then
-    echo "Error: Commit operation failed."
-    exit 1
-fi
-
 git push origin main
-
-if [ $? -ne 0 ]; then
-    echo "Error: Push operation failed."
-    exit 1
-fi
-
-echo "Operation completed successfully."
 
 exit 0
 
