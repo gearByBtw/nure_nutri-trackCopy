3b0d5f25c88af960ecaaee7e1ba98c4e4df00426
Daryna <darinahanina04@gmail.com>
2024-05-06 16:19:46 +0200

    Added base repository

---
+++ be/WebApi/WebApi/Data/Interfaces/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using WebApi.Models;
+
+namespace WebApi.Data.Interfaces;
+
+public interface IRepository<T> where T : BaseModel
+{
+    Task<IEnumerable<T>> GetListAsync(
+        Expression<Func<T, bool>> filter = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null);
+        
+    Task<List<T>> ListAsync(Expression<Func<T, bool>> expression);
+        
+    Task<T> GetByIdAsync(Guid id);
+
+    Task<T> GetByIdAsync(Guid id, string includeProperties);
+
+    Task<T> GetByPredicateAsync(Expression<Func<T, bool>> predicate);
+
+    Task<List<T>> GetListByPredicateAsync(Expression<Func<T, bool>> predicate);
+
+    Task AddAsync(T obj);
+
+    Task UpdateAsync(Guid id, T updateObj);
+
+    Task DeleteAsync(object id);
+}
---
+++ be/WebApi/WebApi/Data/Repository.cs
@@ -0,0 +1,109 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data.Interfaces;
+using WebApi.Models;
+
+namespace WebApi.Data;
+
+public class Repository<T> : IRepository<T> where T : BaseModel
+{
+    protected readonly AppContext _dbContext;
+    private DbSet<T> _dbSet;
+    
+    public Repository(AppContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<T>();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetListAsync(
+        Expression<Func<T, bool>> filter = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null)
+    {
+        IQueryable<T> query = _dbSet;
+
+        if (filter != null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (orderBy != null)
+        {
+            return await orderBy(query).ToListAsync();
+        }
+        else
+        {
+            return await query.ToListAsync();
+        }
+    }
+    
+    public async Task<T> GetByPredicateAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).FirstOrDefaultAsync();
+    }
+    
+    public async Task<List<T>> GetListByPredicateAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public async Task AddAsync(T obj)
+    {
+        await _dbSet.AddAsync(obj);
+        await _dbContext.SaveChangesAsync();
+    }
+    
+    public virtual async Task DeleteAsync(object id)
+    {
+        T entityToDelete = await _dbSet.FindAsync(id);
+        await Delete(entityToDelete);
+    }
+
+    public virtual async Task Delete(T entityToDelete)
+    {
+        if (_dbContext.Entry(entityToDelete).State == EntityState.Detached)
+        {
+            _dbSet.Attach(entityToDelete);
+        }
+        
+        _dbSet.Remove(entityToDelete);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<List<T>> ListAsync(Expression<Func<T, bool>> expression)
+    {
+        return await _dbSet.Where(expression).ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.Where(e => e.Id.Equals(id)).FirstOrDefaultAsync();
+    }
+    
+    public async Task<T> GetByIdAsync(Guid id, string includeProperties)
+    {
+        IQueryable<T> query = _dbSet;
+
+        if (!string.IsNullOrEmpty(includeProperties))
+        {
+            foreach (var includeProperty in includeProperties.Split
+                         (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+        }
+
+        return await query.FirstOrDefaultAsync(e => e.Id.Equals(id));
+    }
+
+    public async Task UpdateAsync(Guid id, T updateObj)
+    {
+        var entity = await GetByIdAsync(id);
+        entity = updateObj;
+        
+        _dbSet.Attach(entity);
+        _dbContext.Entry(entity).State = EntityState.Modified;
+
+        await _dbContext.SaveChangesAsync();
+    }
+}
